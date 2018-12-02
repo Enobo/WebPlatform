1f44a8b01c12a6b640d8d24b70981a429753ed68
inuinana <mail@akihiroinui.com>
2018-12-02 13:47:07 +0100

    Docker導入

---
+++ html/mainPage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--メインページ-->
+<html>
+    <head><!--ヘッダー開始-->
+    <meta charset="utf-8">
+    <title>main_page</title>
+    <!--CSSファイル呼び出し-->
+    <link rel="stylesheet" href="">
+  </head><!--ヘッダー終了-->
+  
+  <body><!--Body開始-->
+    <div class="top_container"><!--top_container開始-->
+        <!--サイドメニュー-->
+        <div class="side_menu">
+            <ul>
+                <li><a href="#">Top</a></li>
+                <li><a href="#">Schedule</a></li>
+                <li><a href="#">GameCenter</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Admin</a></li>
+            </ul>
+        </div>
+        <div class="mainpage_container"><!--top_container開始-->
+            <h1>Welcome to EnoboWorld!</h1>
+        </div><!--top_container終了-->
+    </div>
+  </body><!--Body終了-->
+</html>
